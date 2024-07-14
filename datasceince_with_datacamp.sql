dd91a9155da10762dd8f004bf4e048ac7d9169d9
genius-devops <adamslabaran@gmail.com>
2024-07-14 12:26:45 +0100

    datacamp sql updated

--- datasceince_with_datacamp.sql
+++ datasceince_with_datacamp.sql
@@ -1,5 +1,12 @@
 -- Select the title and release_year of all films released in 1990 or 2000 that were longer than two hours.
+/*
+The "IS NULL" and "IS NOT NULL" operators are useful in SQL (Structured Query Language) to check the presence or absence of a value in a database table.
 
+The "IS NULL" operator is used to determine if a specific column in a database table has null values, meaning there is no data present. It returns true if the column contains null values and false otherwise. This can be helpful when you want to filter out rows that do not have any data for a particular field.
+
+On the other hand, the "IS NOT NULL" operator is used to check if a specific column in a database table has non-null values, indicating that there is data present. It returns true if the column contains non-null values and false otherwise. This can be beneficial when you want to exclude rows that lack data for a particular field.
+
+*/
 -- Take Hint (-10 XP)
 -- 2
 -- Select the title and language of all films in English, Spanish, or French using IN.
