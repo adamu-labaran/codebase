7f6b6dc6498a20aab221c82aad49b00aee0c6ccb
genius-devops <adamslabaran@gmail.com>
2024-07-14 12:11:19 +0100

    sql practice questions

---
+++ q&a.sql
@@ -0,0 +1,7 @@
+-- Why are the IS NULL and IS NOT NULL operators useful? 
+-- The "IS NULL" and "IS NOT NULL" operators are useful in SQL (Structured Query Language) to check the presence or absence of a value in a database table.
+
+-- The "IS NULL" operator is used to determine if a specific column in a database table has null values, meaning there is no data present. It returns true if the column contains null values and false otherwise. This can be helpful when you want to filter out rows that do not have any data for a particular field.
+
+-- On the other hand, the "IS NOT NULL" operator is used to check if a specific column in a database table has non-null values, indicating that there is data present. It returns true if the column contains non-null values and false otherwise. This can be beneficial when you want to exclude rows that lack data for a particular field.
+-- By using these operators, you can effectively query and retrieve data based on whether certain columns have null or non-null values, allowing for more targeted analysis or manipulation of records within your database.
